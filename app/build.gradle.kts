9b3d9cb0c04a172fb86472ea5a35005bc04b1cf3
TanishMoral11 <ishantmoral@gmail.com>
2024-07-28 00:53:31 +0530

    Flag

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,17 +28,26 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.7"
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -51,18 +60,40 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
-    implementation ("com.google.ai.client.generativeai:generativeai:0.1.1")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
-    implementation ("com.itextpdf:itext7-core:7.1.16")
-    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation ("com.google.code.gson:gson:2.8.8")
-    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
-    implementation ("com.airbnb.android:lottie:6.1.0")
-    implementation ("com.github.castorflex.smoothprogressbar:library:1.1.0")
-    implementation ("androidx.cardview:cardview:1.0.0")
 
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
 
+    // Google AI
+    implementation("com.google.ai.client.generativeai:generativeai:0.1.1")
 
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+
+    // iText PDF
+    implementation("com.itextpdf:itext7-core:7.1.16")
+
+    // Gson
+    implementation("com.google.code.gson:gson:2.8.8")
+
+    // Kotlinx Serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+
+    // Lottie
+    implementation("com.airbnb.android:lottie:6.1.0")
+
+    // SmoothProgressBar
+    implementation("com.github.castorflex.smoothprogressbar:library:1.1.0")
+
+    // CardView
+    implementation("androidx.cardview:cardview:1.0.0")
+
+    // Lifecycle and Compose
+    def lifecycle_version = "2.6.1"
+    def compose_version = "1.4.3"
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycle_version")
+    implementation("androidx.compose.runtime:runtime-livedata:$compose_version")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycle_version")
+    implementation("androidx.compose.runtime:runtime:$compose_version")
 }
